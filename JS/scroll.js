43a51f290d1cb19a59fcfb062b5e38cd2848487f
Carvalhols <lorrancdasilva@gmail.com>
2025-12-05 15:36:33 -0300

    Commit home
    Links para outras paginas

---
+++ JS/scroll.js
@@ -0,0 +1,13 @@
+ScrollReveal().reveal('.sr-fade', {
+  duration: 900,
+  distance: '0px',
+  opacity: 0,
+  easing: 'ease-out',
+});
+
+ScrollReveal().reveal('.sr-up', {
+  duration: 900,
+  distance: '40px',
+  origin: 'bottom',
+  easing: 'ease-out',
+});
